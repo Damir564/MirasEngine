c509c9ad1b4db4d45c2c51ddb165d9cb7ac8b8f0
Damir564 <ddamir.nizamutdinov@yandex.ru>
2026-03-23 18:53:38 +0300

    change cmakelists so it will copy config and exe of ifcconvert to the build directory

--- src/engine/IfcConverter.h
+++ src/engine/IfcConverter.h
@@ -6,28 +6,24 @@
 
 #define IFC_CONVERT_PARAMS " --weld-vertices --use-element-names --y-up" /*  --weld-vertices --use-element-types*/
 
-static std::string loadIfcParams() {
-    std::string params = IFC_CONVERT_PARAMS;  // defaults
-
-    std::ifstream file("ifcparse.config");
-    if (file.is_open()) {
-        std::string line;
-        std::getline(file, line);
-        if (!line.empty()) {
-            params = " " + line;
-            std::cout << "[IFC] Config params: " << params << "\n";
-        }
-    }
-
-    return params;
-}
 
 class IfcConverter {
 public:
-    static void setExecutablePath(const std::string& path) { s_path = path; }
 
-    static bool isAvailable() {
-        return std::system((s_path + " --version > /dev/null 2>&1").c_str()) == 0;
+    static std::string loadIfcParams() {
+        std::string params = IFC_CONVERT_PARAMS;  // defaults
+
+        std::ifstream file(s_config);
+        if (file.is_open()) {
+            std::string line;
+            std::getline(file, line);
+            if (!line.empty()) {
+                params = " " + line;
+                std::cout << "[IFC] Config params: " << params << "\n";
+            }
+        }
+
+        return params;
     }
 
     static std::optional<std::string> toGlb(const std::string& ifcPath) {
@@ -69,5 +65,6 @@ public:
     }
 
 private:
-    static inline std::string s_path = "IfcConvert";
+    static inline std::string s_path = (std::filesystem::path("external") / "IfcConvert.exe").string();
+    static inline std::string s_config = (std::filesystem::path("external") / "IfcConvert.config").string();
 };
